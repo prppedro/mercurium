9a89790287643dd54872aa56c99bbe804f9d7bf7
prppedro <prppedro@gmail.com>
2017-08-14 15:37:50 -0300

    Funcionamento parcial do plugin de horários de partida para linhas de ônibus paulistanas

--- plugins/partidas.py
+++ plugins/partidas.py
@@ -1,7 +1,13 @@
 from api import send_message, send_photo
 import datetime
+import re
+from selenium import webdriver
 import requests
 import bs4
+
+# Este plugin acessa as informações de partida de linhas no site da SPTrans,
+# ou, melhor dizendo, no Geologística deles, atualizado para caramba...
+# Leia-se: http://itinerarios.sptrans.com.br/PlanOperWeb/
 
 def on_msg_received(msg, matches):
     # Bem-vindo à bagunça
@@ -18,20 +24,41 @@ def on_msg_received(msg, matches):
     linha = str(matches.group(1))
     variante = str(matches.group(2))
     tipoDia = str(matches.group(3)) # se especificado, 0 = útil, 1 = sáb., 2 = dom.
+
+    # Primeiro eu preciso de um cookie que você consegue acessando o root do
+    # PlanOperWeb. Esse cookie tem um nome bem bacana: "ASPSESSIONIDACBTSSDT"
+    # e sem ele o resultado é um erro do MSSQL. Vá entender...
+    session = requests.Session()
+    session.get("http://itinerarios.sptrans.com.br/PlanOperWeb/")
+
+    # Por algum motivo que me é desconhecido, as coisas não funcionam se eu não
+    # fizer este request aqui, que, talvez deva "legitimar" o Cookie, não sei...
+    session.get("http://itinerarios.sptrans.com.br/PlanOperWeb/ABInfGrConGWEB.asp?MODULO=WEB")
+
     # Nessa parte, eu só preciso obter o código da tabela de partidas...
     url = "http://itinerarios.sptrans.com.br/PlanOperWeb/ABInfSvLinG.asp?SvLinCodigo={}&TpLinCodigo={}".format(linha, variante)
-    output = requests.get(url)
+    output = session.get(url)
     ensopado = bs4.BeautifulSoup(output.content, "html.parser")
-    definicao_raw = ensopado.find_all("div", id="definicao")
+    #print(str(ensopado))
+    #print(output.request.headers)
 
-    # TODO: implementar esta filtragem
-    # Regex: CdPjOID=(\d+)
-    idTabela = "" # Com alguma bruxaria vou ter de obter isso aqui do meio da resposta que vier aí acima
+    # Procura o parâmetro do qual precisamos
+    expr = re.compile("CdPjOID=(\\d+)")
+    match = expr.search(str(ensopado))
+
+    #
+    if not match:
+        send_message(msg["chat"]["id"], str("Linha não encontrada! "))
+        return None
+
+    idTabela = match.group(1) # Não confundir com "matches"
+                              # Recebe a id da qual precisamos
 
     # Caso não haja valor determinado para o dia, procura com
     # base no dia atual...
     diaDeHoje = datetime.datetime.today().weekday()
-    if tipoDia == "":
+    if tipoDia == "None": # Eu não sei porque isso vem como "None" ao
+                          # inves de uma string vazia... Mas funciona...
         if diaDeHoje == 6: # domingo
             tipoDia = "2"
         elif diaDeHoje == 5: # sábado
@@ -41,12 +68,54 @@ def on_msg_received(msg, matches):
 
     # Informação interessante: a data vigente é requerida, porém não usada...
     # Ele retornará a mais recente de maneira qualquer
-    url2 = "http://itinerarios.sptrans.com.br/PlanOperWeb/ABInfSvHorD.asp?CdPjOID={}&CdPjODataVig=01/01/1970&MODULO=&TpDiaID={}" \
-    .format(idTabela, tipoDia)
+    #url = "http://itinerarios.sptrans.com.br/PlanOperWeb/ABInfSvHorD.asp?CdPjOID={}&CdPjODataVig=01/01/1970&MODULO=&TpDiaID={}" \
+    #.format(idTabela, tipoDia)
+    # Encontrei um outro endpoint cuja formatação estava mais correta...
+    # Este endpoint ainda é usado pelo site principal
+    # E ele ignora o parâmetro TpDiaID, usando TpDiaIDpar no lugar...
+    url = "http://itinerarios.extapps.sptrans.com.br/PlanOperWeb/detalheLinha.asp?" + \
+        "CdPjOID={}&TpDiaIDpar={}".format(idTabela, tipoDia)
+    print(url)
+    output = session.get(url)
+    ensopado = bs4.BeautifulSoup(output.content, "html.parser")
 
-    # O PlanOperWeb parou de funcionar enquanto estava programando este
-    # script... Está dropando instantaneamente, típico sintoma de "puxaram
-    # o IIS da tomada"... Se o script ficar por aqui é porque o sistema
-    # deles morreu de vez... 
+    saida = "W.I.P."
 
-    send_message(msg["chat"]["id"], str("W.I.P."))
+    horarios = ensopado.find("table", class_="tabelaHorarios") # Localiza a tabela
+    if horarios:
+
+        letreiroIda = ensopado.find("span", id="lblLetreiroTP").getText()
+        letreiroVolta = ensopado.find("span", id="lblLetreiroTS").getText()
+
+        dia = "Dias úteis"
+        if diaDeHoje == 5:
+            dia = "Sábados"
+        elif diaDeHoje == 6:
+            dia = "Domingos e feriados"
+        saida = "*{}-{}* - *{}* ({}) \nHorários para a viagem de ida, a partir de _{}_\n\n" \
+            .format(linha, variante, letreiroIda, dia, letreiroVolta)
+
+        for tr in horarios.findAll("tr"):
+            tds = tr.findAll("td")
+            if tds:
+                saida += tds[2].getText() + "\n"
+
+
+    else: # Se a tabela não foi encontrada, deve ter rolado algum erro
+        erro = ""
+
+        # TODO: Implementar um tratamento decente de erros
+        errlines = ensopado.findAll("font")
+        if errlines is not None:
+            erro = str(ensopado.getText())
+            print(erro)
+        else:
+            erro = "Uma resposta inesperada fora recebida: \n"
+            erro += str(ensopado.getText())
+
+        send_message(msg["chat"]["id"], erro)
+
+    print(output.request.headers)
+
+    print(saida)
+    send_message(msg["chat"]["id"], saida)
