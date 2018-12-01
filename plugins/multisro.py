5fa08165c32875eb0a4e51bc0365f6f4db8910e7
prppedro <prppedro@gmail.com>
2018-12-01 03:45:21 -0200

    Corrigido o plugin multisro, que quebrou por causa de alguma alteração no site dos correios Usa, agora, o site do MDLivre. Poderia melhorar o código, mas não estou com tempo. Então fica assim até quebrar de novo. Só para, aí sim, consertá-lo.

--- plugins/multisro.py
+++ plugins/multisro.py
@@ -35,13 +35,14 @@ def on_msg_received(msg, matches):
 
 def query(obj):
     '''obj: String'''
-    url = "http://www2.correios.com.br/sistemas/rastreamento/newprint.cfm"
-    headers = { 'User-Agent' : 'Mercurium/0.1 (+http://www.tadeu.org/)' }
-    # Aparentemente, a ECT ainda não entendeu a existência do UTF-8
-    data = { 'objetos' : obj.encode("iso-8859-1")}
+    url = "http://www.mdlivre.com.br/tracking/view/" + obj;
+    headers = \
+        {
+            'User-Agent' : 'Mozilla/5.0 (Mercurium/0.1; +http://www.aehoo.net)'
+        }
 
     try:
-        resposta = requests.post(url, data, headers = headers)
+        resposta = requests.post(url, headers = headers)
     except ConnectionResetError:
         return "HTTP: Estourado o tempo limite de resposta"
     except ConnectionAbortedError:
@@ -54,10 +55,10 @@ def query(obj):
         try:
             parseado = bs4.BeautifulSoup(resposta.content, "html.parser")
             # Delega o trabalho “pesado” ao nosso método montaSaida()
-            return montaSaida(parseado)
+            return montaSaida(parseado, obj)
 
-        except Exception:
-            return "Processamento: Erro aleatório"
+        except Exception as erro:
+            return "*Erro no Python:* " + str(erro)
 
 def S10ok(codigo):
     '''
@@ -95,19 +96,25 @@ def S10ok(codigo):
     else:
         return False
 
-def montaSaida(parseado):
+def montaSaida(parseado, obj):
     ''' Recebe o parseado do método query e monta a saída para a pesquisa'''
-    # Este método, em potencial, recebe uma porrada de coisas praticamente co-
-    # ladas da implementação original. Talvez fosse o caso de refatorá-las,
-    # portando:
-    # TODO: Refatorar isto
+    # Comecei a scrapar a página do MDLivre, porque a dos correios
+    # estava sem condições de tanto cookie que tinha que forçar.
 
-    # Este título h4 só aparece quando o pacote não existe
-    if parseado.find("body").find("h4") is not None:
-        return format("SRO: Este pacote ({}) não retornou resultados", obj)
+    # Isto só aparece quando as coisas não funcionam
+    if parseado.find("body").find("div", class_="text-danger") is not None:
+        return format("*SRO:* O pacote " + obj + " não retornou resultados. ")
     else:
         status = ""
-        tabelita = parseado.find("table", class_="listEvent sro").findAll("tr")
+
+        #print("\n\n\n")
+        #print(parseado.contents)
+        #print("\n\n\n")
+
+        try:
+            tabelita = parseado.find("table", class_="hover").findAll("tr")
+        except Exception:
+            return("*Erro no scraping:* A página não continha uma estrutura esperada pelo parser. ");
 
         # Corre todas as linhas da tabela
         # Por sorte, ao contrário da implementação do Aulete, estas tabelas têm
@@ -118,23 +125,21 @@ def montaSaida(parseado):
             # retorna um array contendo várias strings da segunda coluna
             natureza = evento.findAll("td")[1].findAll(text=True)
 
-            # O lascado, agora, é que a porcaria da interface dos correios usa <label>
-            # em alguns nomes de cidade. O que significa que o índice dois em diante é,
-            # geralmente, uma bagunça... O jeito é concatená-los e arrancar os espaços
-            # em branco...
             status += "`" + localhora[1].strip() + " " + localhora[0].strip() + "`"
             status += "\n"
 
             if len(natureza) > 3:
                 status += "*{} {}*".format(natureza[1].strip(), natureza[3].strip())
             else:
-                status += "*{}*".format(natureza[1].strip())
+                status += "*{}: {}*".format(natureza[0].strip(), natureza[1].strip())
             status += "\n"
 
+            # Fiz uma adaptação porca, com sono, e nem sei o quão longe vai,
+            # mas funciona e é isso aí...
             if len(localhora) > 4:
-                status += "_" + localhora[3].strip() + "_"
+                status += "_" + localhora[1].strip() + "_"
             else:
-                status += "_" + localhora[2].strip() + "_"
+                status += "_" + localhora[3].strip() + "_"
 
             status += "\n\n"
 
